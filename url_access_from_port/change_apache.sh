612f8a553dd874a003b578ed01bce9d796baa993
Serge Ayissi <ayissi.serge@gmail.com>
2019-04-24 17:27:35 -0400

    Create change_apache.sh

---
+++ url_access_from_port/change_apache.sh
@@ -0,0 +1,21 @@
+# Change Apache default port to a custom port
+# For Ubuntu
+
+# Edit ports.conf
+sudo vi /etc/apache2/ports.conf
+
+# replace Listen 80 by
+Listen 0.0.0.0:80
+
+# Or any port chosen to access the API, example 8080
+Listen 0.0.0.0:8080
+
+# Then if needed change 000-default.conf
+sudo vi /etc/apache2/sites-enabled/000-default.conf
+<VirtualHost *:8090>
+# save the file
+[CTRL] + ZZ
+
+# restart Apache
+sudo service apache2 restart
+
