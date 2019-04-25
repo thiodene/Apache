6cf0ce1cebdd52180c21a9a4625e54c28723a3c4
Serge Ayissi <ayissi.serge@gmail.com>
2019-04-25 09:49:37 -0400

    Create change_php_header.php

---
+++ url_access_from_port/change_php_header.php
@@ -0,0 +1,15 @@
+<?php
+
+// Source:
+// https://stackoverflow.com/questions/20035101/why-does-my-javascript-get-a-no-access-control-allow-origin-header-is-present
+
+// For allowing remote connection to API add the following header:
+header('Access-Control-Allow-Origin: *') ;
+
+// rest of the php script
+// ..........
+// ..........
+// ..........
+
+
+?>
